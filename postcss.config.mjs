952bfdd23c5c754f07d6a75f7269703f9ca66fb4
stevenhou7245 <curryhsq7245@gmail.com>
2026-04-04 16:12:12 +0800

    Fix npm problems3

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,7 @@
-import tailwindcss from "@tailwindcss/postcss";
-
-export default {
-  plugins: [tailwindcss()],
+const config = {
+  plugins: {
+    "@tailwindcss/postcss": {},
+  },
 };
+
+export default config;
